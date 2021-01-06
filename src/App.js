451392c77126408c527f140cf88952966fe63c12
karimfaizan <karimfaizan220@gmail.com>
2021-01-06 23:43:53 +0500

    added div and h2

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, placeat quo pariatur dolorem distinctio sunt totam consectetur veniam enim consequuntur dolor, 
+   <div>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, placeat quo pariatur dolorem distinctio sunt totam consectetur veniam enim consequuntur dolor, 
           doloribus id quos saepe maxime suscipit iure reiciendis illum.</p>
+   </div>
+   <div>
+     <h2>helloo</h2>
+   </div>
+
     </div>
   );
 }
