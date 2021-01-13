e6da7cfbb15d98f7d5f9418c8324a15058d1c7c0
naveedali <naveedalirehmani321@gmail.com>
2021-01-14 01:49:40 +0500

    Added Home Page :) tehe

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Ouch1 from "./images/ouch1.png"
+import Ouch2 from "./images/ouch2.png"
+import Ouch3 from "./images/ouch3.png"
+import Ouch6 from "./images/ouch6.png"
+import vector from "./images/vectorCreator2.jpg"
 
 function App() {
   return (
-    <div className="App">
-   <div>
-   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, placeat quo pariatur dolorem distinctio sunt totam consectetur veniam enim consequuntur dolor, 
-          doloribus id quos saepe maxime suscipit iure reiciendis illum.</p>
-   </div>
-   <div>
-     <h2>helloo</h2>
-   </div>
-
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, placeat quo pariatur dolorem distinctio sunt totam consectetur veniam enim consequuntur dolor, 
-          doloribus id quos saepe maxime suscipit iure reiciendis illum.</h2>
-    </div>
+    <section className="home">
+        <div className="main">
+          <p>We Are TodoMatic <br/>Bring your team together, Choose a better way to work</p>
+          <button className="loginButton">Sign In</button>
+          <button className="signupButton">Sign Up</button>
+          <div className="icons">
+          <div className="left">
+            <img className="ouch1" src={Ouch1} alt=""/>
+            <img className="ouch2" src={Ouch2} alt=""/>
+            <img className="ouch3" src={Ouch3} alt=""/>
+            <img className="ouch6" src={Ouch6} alt=""/>
+          </div>
+          <div className="right">
+            <img className="vector"src={vector} alt=""/>
+          </div>
+          </div>
+        </div>
+    </section>
   );
 }
 
