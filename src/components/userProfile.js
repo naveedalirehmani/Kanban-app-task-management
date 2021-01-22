a18e2425de5d1ee0717bb755254e380bcbc16661
naveedali <naveedalirehmani321@gmail.com>
2021-01-23 03:44:18 +0500

    create section designed

--- src/components/userProfile.js
+++ src/components/userProfile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./userProfiles.css";
-import maleProfilePicture from "../images/profileFemale.jpg";
-// import Background from "../images/9.jpg"
+import maleProfilePicture from "../images/maleProfile3.jpg";
+import logInSvg from "../images/project_completed.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
@@ -34,19 +34,35 @@ function User(props) {
           </input>
           </div>
         <div className="buttons">
-          <button><FontAwesomeIcon icon={faBell}></FontAwesomeIcon></button>
-          <button><FontAwesomeIcon icon={faPlus}></FontAwesomeIcon></button>
-          <button><FontAwesomeIcon icon={faHome}></FontAwesomeIcon></button>
-          <button><FontAwesomeIcon icon={faQuestionCircle}></FontAwesomeIcon></button>
-          <button><FontAwesomeIcon icon={faUser}></FontAwesomeIcon></button>
+          <button><FontAwesomeIcon icon={faBell}></FontAwesomeIcon><span>Notifications</span></button>
+          <button><FontAwesomeIcon icon={faPlus}></FontAwesomeIcon><span>Create</span></button>
+          <button><FontAwesomeIcon icon={faHome}></FontAwesomeIcon><span>Home</span></button>
+          <button><FontAwesomeIcon icon={faQuestionCircle}></FontAwesomeIcon><span>Help</span></button>
+          <button><FontAwesomeIcon icon={faUser}></FontAwesomeIcon><span>Profile</span></button>
         </div>
         <div className="profile">
-          <p>{props.userObject.name}</p>
+          <p>{props.userObject.email}</p>
           <img src={maleProfilePicture} alt="" className="profileImage" />
         </div>
       </nav>
       <div className="content">
-
+        <div className="create">
+          <form action="">
+            <p>Add A Task</p>
+            <input type="text" className="name"placeholder="Add Task Name Here"/>
+            <input type="text" className="task"placeholder=" EXAMPLE:.... Get Pastries From The Store #Task 9"/>
+            <div className="taskButtons">
+            <button className="add">Add</button>
+            <button className="setDate">Set Date</button>
+            <button className="setPriorityHigh">High</button>
+            <button className="setPriorityMedium">Medium</button>
+            <button className="setPriorityLow">Low</button>
+            </div>
+          </form>
+            <div className='svgAddTask'>
+              <img src={logInSvg} alt=""/>
+            </div>
+        </div>
       </div>
     </section>
   );
